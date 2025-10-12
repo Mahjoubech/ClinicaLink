16d6520c4effc40c0c9ab0ee55c9721b0618bceb
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-12 23:58:18 +0100

    Ajouter le servlet GeneralisteServlet pour gérer l'affichage des généralistes dans l'interface  admin : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/CLIN-27

--- src/main/java/io/github/Mahjoubech/clinicalink/servlet/admin/GeneralisteServlet.java
+++ src/main/java/io/github/Mahjoubech/clinicalink/servlet/admin/GeneralisteServlet.java
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 @WebServlet("/admin/generaliste")
 public class GeneralisteServlet extends HttpServlet {
 
+
     private MedcenServiceInterface medcenService;
    private MedcenDaoInterface medao;
     @Override
